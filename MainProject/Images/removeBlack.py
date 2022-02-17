20b034978089ebdbf85d5c1a5fb84b82e59cf010
Brett Huffman <dicer2000@yahoo.com>
2022-02-16 23:41:58 -0600

    Checkpoint before formatting images

---
+++ MainProject/Images/removeBlack.py
@@ -0,0 +1,25 @@
+
+from PIL import Image
+from pathlib import Path
+
+result = list(Path(".").rglob("*.[jJ][pP]*"))
+for file in result:
+    print(file)
+
+    exit()
+    # creating a image object
+    # Such as: r"./Ash/ash1.jpg"
+    im = Image.open(file)
+    px = im.load()
+
+    shouldDelete = 1
+    for i in range(18):
+        print (i, px[4, im.height-i-1])
+        if px[4, im.height-i-1][0] > 10 | px[4, im.height-i-1][1] > 10 | px[4, im.height-i-1][2] > 10:
+            shouldDelete = 0
+
+    print(shouldDelete)
+
+    if shouldDelete:
+        im = im.crop((0,0,im.width, im.height-20))
+        im.save(file)
